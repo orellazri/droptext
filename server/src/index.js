f64e436a2e50a48ddf7775ae1f0f00f64e03ceeb
Orel <ryiseld@gmail.com>
2021-08-05 19:27:48 +0300

    Add react client

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,30 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("welcome to droptext");
+});
+
+app.post("/create", (req, res, next) => {
+  try {
+    const { content } = req.body;
+    if (!content) {
+      throw new Error("Content missing");
+    } else {
+      res.json({ message: "OK!" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log("Error :" + err);
+  res.status(400).json({
+    message: err.message,
+  });
 });
 
 const port = process.env.PORT || 3000;
