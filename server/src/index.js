89949f6620d652f52e304b58275f7cfbbcdcd446
Orel <ryiseld@gmail.com>
2021-08-06 15:49:10 +0300

    Change default server port

--- server/src/index.js
+++ server/src/index.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
