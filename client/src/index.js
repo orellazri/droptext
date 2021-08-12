ca271f903de4d65a2d49d05078b1b7f09f84988f
Orel <ryiseld@gmail.com>
2021-08-12 22:23:05 +0300

    Change default port in axios

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
