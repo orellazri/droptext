155f472a0d98ef9c0bf56e84c2c2a5c2247329e9
Orel <ryiseld@gmail.com>
2021-08-14 19:24:45 +0300

    Remove badger and add mongo

--- server/go.mod
+++ server/go.mod
@@ -5,4 +5,7 @@ go 1.16
 require (
 	github.com/dgraph-io/badger/v3 v3.2103.1 // indirect
 	github.com/gin-gonic/gin v1.7.3
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
-	badger "github.com/dgraph-io/badger/v3"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var db *badger.DB
+type Request struct {
+	content string `json:"content" binding:"required"`
+}
+
+var ctx context.Context
+var collection *mongo.Collection
 
 func main() {
-	db, err := badger.Open(badger.DefaultOptions("./badger"))
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URL")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection = client.Database("droptext").Collection("texts")
 
 	router := gin.Default()
 
@@ -24,8 +50,6 @@ func main() {
 	router.POST("/create", postCreate)
 
 	router.Run("localhost:8080")
-
-	defer db.Close()
 }
 
 func getIndex(c *gin.Context) {
@@ -35,43 +59,49 @@ func getIndex(c *gin.Context) {
 }
 
 func getById(c *gin.Context) {
-	// id := c.Param("id")
-
-	err := db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte("id"))
-		if err != nil {
-			return err
-		}
-
-		err = item.Value(func(val []byte) error {
-			fmt.Printf("The value is %s\n", val)
-			return nil
-		})
-
-		if err != nil {
-			sendError(c, err)
-		}
-
-		return nil
-	})
-
+	id := c.Param("id")
+	err := collection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}})
 	if err != nil {
-		sendError(c, err)
+		sendError(c, "ID Not found in database")
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "OK!",
+		"message": "get by id!",
 	})
 }
 
 func postCreate(c *gin.Context) {
+	// Generate unique ID
+	var id string
+	var mongoErr *mongo.SingleResult = nil
+	var err error
+
+	for mongoErr == nil {
+		id, err = gonanoid.New(10)
+		if err != nil {
+			sendError(c, "Could not generate nanoid")
+			return
+		}
+		mongoErr = collection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}})
+	}
+
+	var request Request
+	if err = c.BindJSON(&request); err != nil {
+		sendError(c, "Could not bind JSON")
+		return
+	}
+	fmt.Printf("Content is : %s\n", request.content)
+
+	// res, err := collection.InsertOne(ctx, bson.D{primitive.E{Key: "_id", Value: id}, {Key: "content", Value: }})
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Created",
+		"message": "found free id: " + id,
 	})
 }
 
-func sendError(c *gin.Context, err error) {
+func sendError(c *gin.Context, err string) {
+	// TODO: Check if in production/development to know which error to send
 	c.JSON(http.StatusBadRequest, gin.H{
 		"message": fmt.Sprintf("An error occured: %v", err),
 	})
