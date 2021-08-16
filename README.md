685e0a8a4b8927263d3534bac28458e55400da3f
Orel <ryiseld@gmail.com>
2021-08-16 15:59:38 +0300

    Add actix server

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Web app to send encrypted text fast.
 
-Uses Node.js & Express for the server, React for the client and levelup as a persistent key-value database.
+Uses Rust & Actix for the server, MongoDB for the database and React for the client.
 
 After submitting your text, it gets encrypted locally, and a private key is also generated locally. The encrypted contents are sent to the server. The server sends back an ID, which lets you can share your url that consists of the ID and the private key.
 
@@ -12,6 +12,13 @@ Demo: https://droptext.netlify.app/
 
 ## Usage
 
-In both the server and client directories, copy .env.example to .env and change the parameters accordingly.
+For both the server and the user, copy `.env.example` to `.env` and set the parameters to your needs.
 
-To run the server/client execute `yarn start` (or npm) in the respective directory.
+### Server
+
+Navigate to the `server` directory and run `cargo run` or `cargo run --release`
+
+### Client
+
+Navigate to the `client` directory and run `yarn install` and `yarn start` (or npm if you prefer).
+In both the server and client directories, copy .env.example to .env and change the parameters accordingly.
---
+++ server/src/main.rs
@@ -0,0 +1,111 @@
+#[macro_use]
+extern crate actix_web;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use mongodb::bson::{doc, Bson};
+use mongodb::{options::ClientOptions, Client, Collection};
+use nanoid::nanoid;
+use serde::{Deserialize, Serialize};
+use std::env;
+
+struct AppState {
+    pub collection: Collection,
+}
+
+#[derive(Deserialize)]
+struct Drop {
+    content: String,
+}
+
+#[derive(Serialize)]
+struct CreateResponse {
+    id: String,
+}
+
+#[derive(Serialize)]
+struct GetResponse {
+    content: String,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("welcome to droptext")
+}
+
+#[get("/{id}")]
+async fn get_by_id(web::Path(id): web::Path<String>, state: web::Data<AppState>) -> impl Responder {
+    let find = state.collection.find_one(doc! {"_id": &id}, None).await;
+    match find {
+        Ok(res) => match res {
+            Some(doc) => {
+                if let Some(content) = doc.get("content").and_then(Bson::as_str) {
+                    HttpResponse::Ok().json(GetResponse {
+                        content: content.to_string(),
+                    })
+                } else {
+                    HttpResponse::BadRequest()
+                        .body("Could not find drop with the given id".to_string())
+                }
+            }
+            None => {
+                HttpResponse::BadRequest().body("Could not find drop with the given id".to_string())
+            }
+        },
+        Err(e) => HttpResponse::BadRequest().body(format!("Error: {}", e)),
+    }
+}
+
+#[post("/create")]
+async fn create_drop(req_body: String, state: web::Data<AppState>) -> impl Responder {
+    let deserialized: Drop = serde_json::from_str(&req_body).unwrap();
+
+    if deserialized.content.trim().is_empty() {
+        HttpResponse::BadRequest().body("Invalid content".to_string());
+    }
+
+    let mut id: String;
+    loop {
+        id = nanoid!(10);
+        let find_id = state.collection.find_one(doc! {"_id": &id}, None).await;
+        match find_id {
+            Ok(res) => match res {
+                Some(_) => continue,
+                None => break,
+            },
+            Err(e) => {
+                HttpResponse::BadRequest().body(format!("Error: {}", e));
+            }
+        }
+    }
+
+    state
+        .collection
+        .insert_one(doc! {"_id": &id, "content": deserialized.content}, None)
+        .await
+        .expect("Could not insert to database");
+
+    HttpResponse::Ok().json(CreateResponse { id: id.to_string() })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let mongo_url = env::var("MONGO_URL").expect("MONGO_URL environment variable not set!");
+    let options = ClientOptions::parse(&mongo_url)
+        .await
+        .expect("Could not create mongodb client options");
+    let client = Client::with_options(options).expect("Could not connect to mongodb");
+
+    HttpServer::new(move || {
+        App::new()
+            .data(AppState {
+                collection: client.database("droptext").collection("drops"),
+            })
+            .service(index)
+            .service(get_by_id)
+            .service(create_drop)
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
+}
